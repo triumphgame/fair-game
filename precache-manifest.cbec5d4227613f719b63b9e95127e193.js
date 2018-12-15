d785b18ac011bf80df28fde973d25a005d2952b2
tunghm94 <tunghm@cardano-labo.com>
2018-12-15 18:38:21 +0700

    Updates

---
+++ precache-manifest.cbec5d4227613f719b63b9e95127e193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/triumphgame/fair-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d2c267e1fd3a03c31ccb",
+    "url": "/triumphgame/fair-game/static/js/runtime~main.d2c267e1.js"
+  },
+  {
+    "revision": "567614a04b9ffc2c10b9",
+    "url": "/triumphgame/fair-game/static/js/main.567614a0.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/triumphgame/fair-game/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "567614a04b9ffc2c10b9",
+    "url": "/triumphgame/fair-game/static/css/main.1f27fd52.chunk.css"
+  },
+  {
+    "revision": "13832a0cd88c95ea19d266fb0f94fb9e",
+    "url": "/triumphgame/fair-game/index.html"
+  }
+];
